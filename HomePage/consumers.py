cc73424aff5bae7ee3778e563a922457009ad293
Eremeev Vlad <eremeev.vlad.v@yandex.ru>
2022-12-03 23:26:25 +0300

    ASync working version

--- HomePage/consumers.py
+++ HomePage/consumers.py
@@ -1,47 +1,49 @@
+import copy
 import json
 
+from asgiref.sync import sync_to_async
+from channels.db import database_sync_to_async
 from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 
 from .models import Participant, MeetingParticipants, Meeting
 
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import WebsocketConsumer, AsyncWebsocketConsumer
 
 
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
-        self.accept()
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        await self.accept()
 
-    def disconnect(self, close_code):
+    async def disconnect(self, close_code):
         pass
 
-    def receive(self, text_data=None, bytes_data=None):
-        if text_data:
+    async def receive(self, text_data=None, bytes_data=None):
+        if text_data is not None:
             text_data_json = json.loads(text_data)
             image = text_data_json["webcam"]
             person_id = text_data_json["person"]
             meeting = text_data_json["meeting"]
             if image:
                 try:
-                    participant = Participant.objects.get(person=person_id)
+                    participant = await Participant.objects.aget(person=person_id)
                     participant.webcam_meta = image
-                    participant.save()
+                    await sync_to_async(participant.save)()
                 except ObjectDoesNotExist:
                     print("Объект не сушествует")
                 except MultipleObjectsReturned:
                     print("Найдено более одного объекта")
-            self.send_data(meeting)
+            await self.send_data(meeting)
 
-    def send_data(self, meeting_link):
-        meeting = Meeting.objects.filter(link=meeting_link)
-        meeting_participants = MeetingParticipants.objects.filter(meeting=meeting[0].id)
+    async def send_data(self, meeting_link):
+        meeting = await Meeting.objects.filter(link=meeting_link).aget()
         data = {}
-        for meeting_participant in meeting_participants:
-            person = Participant.objects.get(id=meeting_participant.person.id)
-            data[person.person.first_name + "_" + person.person.last_name] = {
+        async for meeting_participant in MeetingParticipants.objects.filter(meeting=meeting.id):
+            person = await Participant.objects.aget(id=meeting_participant.person_id)
+            data[person] = {
                 'mic': person.mic,
                 'spk': True,
                 'webcam': person.webcam,
                 'mic_meta': person.mic_meta,
                 'webcam_meta': person.webcam_meta
             }
-        self.send(text_data=json.dumps(data))
+        await self.send(text_data=json.dumps(data))
